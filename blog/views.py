c045231352d9ad3091e62cedfe0ecec64b21b43c
Adewale Adeleye <Adewagold@yahoo.com>
2021-01-24 22:13:37 +0100

    Add master template

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
+posts = [{
+    'author':'Adewale', 
+    'title':'The Prince of Mars',
+    'content':'Nullam quis risus eget urna mollis ornare vel eu leo. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nullam id dolor id nibh ultricies vehicula.', 
+    'date_posted':'January 21,2020'
+},
+{
+    'author':'Adeleye', 
+    'title':'Welcome to Saturn',
+    'content':'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer posuere erat a ante.', 
+    'date_posted':'January 21,2020'
+}]
+
+
 def home(request):
-    return HttpResponse("<h1>Welcome to the blog </h1>")
+    context = {
+        'posts':posts,
+        'title':'Home - Chatty-web'
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
-    return HttpResponse("<h1>Welcome to the about section of the blog </h1>")
+    return render(request, 'blog/about.html')
 
