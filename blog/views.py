ea80100c3b35bd7e488d8be75d74256470053e28
Adewale Adeleye <Adewagold@yahoo.com>
2021-01-27 10:20:38 +0100

    Update Posts

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import Post
-
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin
 
 posts = Post.objects.all()
 
@@ -12,6 +13,36 @@ def home(request):
         'title':'Home - Chatty-web'
     }
     return render(request, 'blog/home.html', context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/home.html" # app / model_viewtype.html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author= self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self, form):
+        form.instance.author= self.request.user
+        return super().form_valid(form)
+
 
 def about(request):
     return render(request, 'blog/about.html')
