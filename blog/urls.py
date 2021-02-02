f794f83c9d4daa49444695b4952cba304c9a32d7
Adewale Adeleye <Adewagold@yahoo.com>
2021-02-02 19:47:57 +0100

    Add pagination to posts

--- blog/urls.py
+++ blog/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from . import views
-from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView
+from .views import PostListView, PostDetailView, PostCreateView, PostUpdateView, PostDeleteView, UserPostListView
 
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
     path('post/<int:pk>/', PostDetailView.as_view(), name="post-detail"),
+    path('post/<str:username>/', UserPostListView.as_view(), name="user-posts"),
     path('post/new/', PostCreateView.as_view(), name="post-create"),
     path('post/update/<int:pk>/', PostUpdateView.as_view(), name="post-update"),
     path('post/delete/<int:pk>/', PostDeleteView.as_view(), name="post-delete"),
--- django_project/urls.py
+++ django_project/urls.py
@@ -25,6 +25,9 @@ urlpatterns = [
     path('register/', user_views.register, name='register'),
     path('profile/', user_views.profile, name='profile'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('password-reset-confirm/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name='users/password_reset.html'), name='password-reset'),
+    path('password-reset/done', auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), name='password-reset-done'),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
     path('', include('blog.urls')),
 ] 
